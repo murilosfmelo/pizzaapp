71e7aa397d85d41897d1aae879ea8ad1830cbacd
MURILO SABINO FARIAS DE MELO <murilo.sfmelo@senacsp.edu.br>
2023-06-22 17:14:45 -0300

    Trabalho quase completo

--- app/Models/ClienteEndereco.php
+++ app/Models/ClienteEndereco.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
-class Endereco extends Model
+use App\Models\{Cliente,Endereco};
+class ClienteEndereco extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $table = 'enderecos';
-    protected $primaryKey = 'id_endereco';
+    protected $table = 'clientes_enderecos';
+    protected $primaryKey = 'id_cliente_endereco';
     protected $dates = [
      'created_at',
      'updated_at',
@@ -22,14 +22,8 @@ class Endereco extends Model
 
     //campos que podem ser viualizados/manipulados fora da classe
     protected $fillable = [
+        'id_cliente',
         'id_endereco',
-        'endereco',
-        'numero',
-        'complemento',
-        'bairro',
-        'cidade',
-        'uf',
-        'cep',
         'observacoes'
     ];
 
@@ -43,15 +37,21 @@ class Endereco extends Model
      */
 
 
-    // public function tipo() : object {
+     public function cliente() : object {
 
-    //        return $this->hasOne(TipoProduto::class,
-   //                                 'id_tipo_produto',
-    //                                'id_tipo_produto');
+            return $this->hasOne(Cliente::class,
+                                    'id_cliente',
+                                    'id_cliente');
 
-   //  }
+     }
 
+     public function endereco() : object {
 
+        return $this->hasOne(Endereco::class,
+                                'id_endereco',
+                                'id_endereco');
+
+ }
 
 
 }
